a5a7d75b2b5ee8305e9c8531b8a1aac6ffef83c8
Yannick COUSIN <94236356+yannick-cousin@users.noreply.github.com>
2022-03-16 11:49:14 +0100

    First Commit !

---
+++ back/index.js
@@ -0,0 +1,24 @@
+const cors = require('cors');
+const express = require('express');
+//const morgan = require('morgan'); 
+const bodyParser = require('body-parser');
+require('dotenv').config();
+const connection = require('./db.js');
+const PORT = process.env.PORT || 8000;
+
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+app.use('/public', express.static('public'));
+
+app.listen(PORT, (err) => {
+	// eslint-disable-next-line no-console
+	if (err) console.error(err);
+	// eslint-disable-next-line no-console
+	console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
