b9e990d48eb14f8e02948ccf25ef72720ef654ed
Yannick COUSIN <94236356+yannick-cousin@users.noreply.github.com>
2022-03-17 20:41:10 +0100

    Update

--- front/src/App.js
+++ front/src/App.js
@@ -1,34 +1,35 @@
-import './App.css'
-import { Routes, Route } from 'react-router-dom'
-import { useAuth0 } from '@auth0/auth0-react'
-import Connexion from './screens/Connexion'
-import Game from './screens/Game'
-import Home from './screens/Home'
-import Prevention from './screens/Prevention'
-import LastPage from './screens/LastPage'
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
+import Connexion from './screens/Connexion';
+import Game from './screens/Game';
+import Home from './screens/Home';
+import Prevention from './screens/Prevention';
+import Quizz from './screens/Quizz';
+import LastPage from './screens/LastPage';
 
 const App = () => {
-  const { isAuthenticated, isLoading } = useAuth0()
+	const { isAuthenticated, isLoading } = useAuth0();
 
-  if (isLoading) {
-    return <div>Loading</div>
-  }
+	if (isLoading) {
+		return <div>Loading</div>;
+	}
 
-  return isAuthenticated ? (
-    <div className='App'>
-      <Routes>
-        <Route path='/connexion' element={<Connexion />} />
-        <Route path='/' element={<Home />} />
-        <Route path='/game' element={<Game />} />
-        <Route path='/preventions' element={<Prevention />} />
-        <Route path='/lastpage' element={<LastPage />} />
-      </Routes>
-    </div>
-  ) : (
-    <>
-      <Connexion />
-    </>
-  )
-}
+	return isAuthenticated ? (
+		<div className="App">
+			<Routes>
+				<Route path="/connexion" element={<Connexion />} />
+				<Route path="/" element={<Home />} />
+				<Route path="/game" element={<Quizz />} />
+				<Route path="/preventions" element={<Prevention />} />
+				<Route path="/lastpage" element={<LastPage />} />
+			</Routes>
+		</div>
+	) : (
+		<>
+			<Connexion />
+		</>
+	);
+};
 
-export default App
+export default App;
