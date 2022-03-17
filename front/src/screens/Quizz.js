04004c95e1372edabc42df4be898eacb3b99f54b
Julien-ArtachoNieto <julien.artachonieto@gmail.com>
2022-03-17 16:43:38 +0100

    Retrait du bouton validation, implémentation de la logique de jeux

--- front/src/screens/Quizz.js
+++ front/src/screens/Quizz.js
@@ -4,16 +4,32 @@ import axios from "axios";
 const Quizz =()=>{
   const [questions,setQuestions]=useState()
   const [selectedQuestion,setSelectedQuestion]=useState('')
-  const [reponses,setReponses]=useState('')
-  const [nbQuestion,setNbQuestion]=useState()
-  const randomQuestion=(questions)=>{
-    if(questions) {
-      let idQuestion = 0
-      idQuestion = Math.floor(Math.random() * questions.length)
-      setSelectedQuestion(questions[idQuestion])
-      console.log('SELECTED QUESTION', selectedQuestion)
-    }
+  const [reponses,setReponses]=useState(0)
+  const [nbQuestion,setNbQuestion]=useState(0)
+  const [score,setScore]=useState(0)
+  const [gameFinished,setGameFinished]=useState(false)
+  const handleValidation=()=>{
+
+
   }
+
+  const handleResult=(reponse)=>{
+    const selectedAnswer = document.querySelector(reponse.value)
+    console.log('HANDLERESULT SELECTEDANSWER QUERYSELECTOR',selectedAnswer)
+    // for(let i=0;selectedAnswer.length;i++){
+    //
+    // }
+    setNbQuestion(nbQuestion+1)
+    if(nbQuestion==1){
+      setGameFinished(true)
+    }
+    if(reponse.trueFalse){
+      setScore(score+1)
+      console.log('Score',score)
+    }
+    console.log(nbQuestion)
+  }
+
   useEffect(()=>{
     if(!questions){
       axios.get(`http://localhost:3030/questions`).
@@ -28,19 +44,19 @@ const Quizz =()=>{
       axios.get(`http://localhost:3030/reponses/${questions[idQuestion].id}`).
       then((res)=>res.data).then((res)=>console.log(res)||setReponses(res))
     }
-  },[questions])
+  },[questions,nbQuestion])
 
 
   return(
     <div>
-      <div className='quizzContainer'>
+      {gameFinished?"":<div className='quizzContainer'>
         <h1 className='title' >Quizz</h1>
         <h2 className='question'>{selectedQuestion.question}</h2>
         <div className='answersContainer'>
-          {reponses?reponses.map(reponse=><button key={reponse.id} className='answer'>{reponse.reponse}</button>):''}
+          {reponses?reponses.map(reponse=><button key={reponse.id} className='answer' onClick={()=>handleResult(reponse)}>{reponse.reponse}</button>):''}
         </div>
-        <button className='validation'>Validation</button>
-      </div>
+
+      </div>}
     </div>
   )
 }
