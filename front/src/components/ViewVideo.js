28d81fa538c55c0761326ecff8f6f89226a8911b
Anouchka <anouchka.maurer@protonmail.com>
2022-03-18 01:15:39 +0100

    changements

--- front/src/components/ViewVideo.js
+++ front/src/components/ViewVideo.js
@@ -1,19 +1,32 @@
-const ViewVideo = (props) => {
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import Footer from '../components/Footer'
+import Header from '../components/Header'
+
+const ViewVideo = () => {
+  const [url, setUrl] = useState()
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:4242/videos`)
+      .then((res) => res.data)
+      .then((res) => setUrl(res))
+  }, [])
+
   return (
-    <div className="viewvideo">
+    <div className='viewvideo'>
+      <Header />
       <iframe
-				src={`https://www.youtube.com/embed/${props.link.map(
-							(el) => el.youtubelink
-						)}`}
-				title="YouTube video player"
-				frameborder="0"
-				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope"
-				allowfullscreen
-				className="player"
-			>
-      </iframe>
+        src={`https://www.youtube.com/embed/${url}`}
+        title='YouTube video player'
+        frameborder='0'
+        allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope'
+        allowfullscreen
+        className='player'
+      ></iframe>
+      <Footer />
     </div>
-  );
+  )
 }
 
-export default ViewVideo;
+export default ViewVideo
